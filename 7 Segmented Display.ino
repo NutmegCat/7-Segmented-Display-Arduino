85461d8b7cb2c5e413531bc5a378e55d75fd5c18
Aaron Prince Anu <77171047+NutmegCat@users.noreply.github.com>
2024-06-13 20:31:07 -0400

    alt code

--- 7 Segmented Display.ino
+++ 7 Segmented Display.ino
@@ -1,366 +1,50 @@
-/*
-The following program displays characters 1 - F in ascending and descending order. 
-It works by turning on and off certain segments of the display.
-The code first runs through characters 1 - F and F - 1, then loops back to the top.
-*/
+// Pin connections for common cathode 7-segment display const int segmentPins[] = {2, 3, 4, 5, 6, 7, 8, G};
 
-void setup() 
-{
-    // Set pins 2 through 8 as outputs
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(7, OUTPUT);
-    pinMode(8, OUTPUT);
+void setup() {
+// Set segment pins as OUTPUT for (int i = 0; i < 8; i++) {
+pinMode(segmentPins[i], OUTPUT);
 }
 
-void loop() 
-{
-    // Ascending Order
-    // Blank display
-    
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    delay(2000); 
-    
-    // Displaying 0
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
+}
+void loop() {
+// Display Hexadecimal digits in ascending order displayHexAscending();
+delay(1000); // Wait for 1 second
 
-    // Displaying 1
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
 
-    // Displaying 2
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 3
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 4
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 5
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying 6
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying 7
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 8
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 9
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying A
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying B
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying C
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying D
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying E
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying F
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Blank display
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    delay(2000);
-
-    // Descending Order
-
-    // Displaying F
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying E
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-    
-    // Displaying D
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying C
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying B
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying A
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 9
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 8
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 7
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 6
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying 5
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    delay(1000);
-
-    // Displaying 4
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 3
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 2
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 1
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // Displaying 0
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    delay(1000);
-
-    // The code will then loop back to the top
+// Display Hexadecimal digits in descending order displayHexDescending();
+delay(1000); // Wait for 1 second
+}
+void displayHexAscending() { for (int i = 0; i <= 15; i++) {
+displayHexDigit(i);
+delay(500); // Display each digit for 0.5 seconds
+}
+}
+void displayHexDescending() { for (int i = 15; i >= 0; i--) {
+ 
+displayHexDigit(i);
+delay(500); // Display each digit for 0.5 seconds
+}
+}
+void displayHexDigit(int digit) {
+// Hexadecimal values for common cathode 7-segment display const int hexValues[][8] = {
+{1, 1, 1, 1, 1, 1, 0, 0}, // 0
+{0, 1, 1, 0, 0, 0, 0, 0}, // 1
+{1, 1, 0, 1, 1, 0, 1, 0}, // 2
+{1, 1, 1, 1, 0, 0, 1, 0}, // 3
+{0, 1, 1, 0, 0, 1, 1, 0}, // 4
+{1, 0, 1, 1, 0, 1, 1, 0}, // 5
+{1, 0, 1, 1, 1, 1, 1, 0}, // 6
+1, 1, 1, 0, 0, 0, 0, 0}, // 7
+{1, 1, 1, 1, 1, 1, 1, 0}, // 8
+{1, 1, 1, 1, 0, 1, 1, 0}, // G
+{1, 1, 1, 0, 1, 1, 1, 0}, // A
+{0, 0, 1, 1, 1, 1, 1, 0}, // B
+{1, 0, 0, 1, 1, 1, 0, 0}, // C
+{0, 1, 1, 1, 1, 0, 1, 0}, // D
+{1, 0, 0, 1, 1, 1, 1, 0}, // E
+{1, 0, 0, 0, 1, 1, 1, 0} // F
+    };
+// Display the specified hexadecimal digit for (int j = 0; j < 8; j++) {
+digitalWrite(segmentPins[j], hexValues[digit][j]);
+    }
 }
